ef0a75f2eadf642b3948cedcf71cfceccf7947bb
Dylan Landry <dylanglandry@gmail.com>
2023-12-11 19:46:55 -0500

    allocate and free color buffer

--- Main.c
+++ Main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <SDL.h>
 
 bool is_running = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+uint32_t* color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -17,8 +21,8 @@ bool initialize_window(void) {
 		NULL,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
+		window_width,
+		window_height,
 		SDL_WINDOW_BORDERLESS
 	);
 	if (!window) {
@@ -37,7 +41,7 @@ bool initialize_window(void) {
 }
 
 void setup(void) {
-
+	color_buffer = malloc(sizeof(uint32_t) * window_width * window_height);
 }
 
 void process_input(void) {
@@ -58,13 +62,21 @@ void process_input(void) {
 
 void update(void) {
 
-}
+} 
 
 void render(void) {
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 1);
 	SDL_RenderClear(renderer);
 
 	SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void) {
+	// Destroys resources in reverse order that they were allocated.
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 int main(int argc, char* args[]) {
@@ -78,6 +90,8 @@ int main(int argc, char* args[]) {
 		update();
 		render();
 	}
+
+	destroy_window();
 
 	return 0;
 }
